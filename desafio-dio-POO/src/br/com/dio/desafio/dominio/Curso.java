3579da38f3bf4406ca0905447651f39a82b5ab19
CamiloMarcos <marcoshcamilo@gmail.com>
2024-07-09 20:00:29 -0300

    commite abstração encapsulamento

---
+++ desafio-dio-POO/src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,46 @@
+package br.com.dio.desafio.dominio;
+
+public class Curso {
+
+    private String titulo;
+    private String descricao;
+    private int cargaHraria;
+
+    public Curso() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCargaHraria() {
+        return cargaHraria;
+    }
+
+    public void setCargaHraria(int cargaHraria) {
+        this.cargaHraria = cargaHraria;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo= " + titulo +
+                ", descricao= " + descricao +
+                ", cargaHraria= " + cargaHraria +
+                '}';
+    }
+}
+
